4517f188c507a9eaccd66b093219d4469379a2da
bala-amypo <bala@amypo.in>
2025-12-22 08:58:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/GarageController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Garage;
+import com.example.demo.service.GarageService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/garages")
+public class GarageController {
+
+    private final GarageService garageService;
+
+    public GarageController(GarageService garageService) {
+        this.garageService = garageService;
+    }
+
+    @PostMapping
+    public Garage createGarage(@RequestBody Garage garage) {
+        return garageService.createGarage(garage);
+    }
+}
