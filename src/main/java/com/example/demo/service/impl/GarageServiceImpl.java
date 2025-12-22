1743c8038ebb0c9b032c223000bb09083030219c
bala-amypo <bala@amypo.in>
2025-12-22 09:01:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/GarageServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Garage;
+import com.example.demo.repository.GarageRepository;
+import com.example.demo.service.GarageService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GarageServiceImpl implements GarageService {
+
+    private final GarageRepository garageRepository;
+
+    public GarageServiceImpl(GarageRepository garageRepository) {
+        this.garageRepository = garageRepository;
+    }
+
+    @Override
+    public Garage createGarage(Garage garage) {
+        if (garageRepository.findByGarageName(garage.getGarageName()).isPresent()) {
+            throw new IllegalArgumentException("Garage already exists");
+        }
+        return garageRepository.save(garage);
+    }
+}
