0e0b63c9d6cdbafe32e28670d0085be50226f35d
bala-amypo <bala@amypo.in>
2025-12-26 16:37:19 +0000

    User commit

--- src/main/java/com/example/demo/repository/ServiceEntryRepository.java
+++ src/main/java/com/example/demo/repository/ServiceEntryRepository.java
@@ -4,22 +4,12 @@ import com.example.demo.model.ServiceEntry;
 import com.example.demo.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface ServiceEntryRepository extends JpaRepository<ServiceEntry, Long> {
 
-    Optional<ServiceEntry> findTopByVehicleOrderByOdometerReadingDesc(Vehicle vehicle);
-
     List<ServiceEntry> findByVehicleId(Long vehicleId);
 
-    // Used conceptually in HQL tests
-    List<ServiceEntry> findByGarageAndMinOdometer(Long garageId, int minOdometer);
-
-    List<ServiceEntry> findByVehicleAndDateRange(
-            Long vehicleId,
-            LocalDate from,
-            LocalDate to
-    );
+    Optional<ServiceEntry> findTopByVehicleOrderByOdometerReadingDesc(Vehicle vehicle);
 }
