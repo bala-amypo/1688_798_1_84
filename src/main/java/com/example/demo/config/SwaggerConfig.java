125a9cf30a1e17bb4c106255a0aa35c435592934
bala-amypo <bala@amypo.in>
2025-12-27 05:31:08 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +11,6 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI vehicleServiceAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("https://9213.pro604cr.amypo.ai").description("Production Server"))
                 .info(new Info()
                         .title("Vehicle Service History API")
                         .description("API for managing vehicle service history")
