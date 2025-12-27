9c3bf78b451654335613a927c4d1827ee33bacbf
bala-amypo <bala@amypo.in>
2025-12-27 04:41:04 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VehicleServiceHistoryApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(VehicleServiceHistoryApplication.class, args);
-    }
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
 }
