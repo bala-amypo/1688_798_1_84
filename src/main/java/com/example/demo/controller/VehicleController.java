71f9d7897a5fa8bbb6e59157fcd535a7a66a8b94
bala-amypo <bala@amypo.in>
2025-12-27 02:52:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/VehicleController.java
+++ src/main/java/com/example/demo/controller/VehicleController.java
@@ -17,11 +17,9 @@ public class VehicleController {
     }
 
     @PostMapping
-    public ResponseEntity<Vehicle> createVehicle(@RequestBody Vehicle vehicle) {
-    Vehicle saved = vehicleService.saveVehicle(vehicle);
-    return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    public Vehicle create(@RequestBody Vehicle v) {
+        return service.createVehicle(v);
     }
-
 
     @GetMapping("/{id}")
     public Vehicle get(@PathVariable Long id) {
