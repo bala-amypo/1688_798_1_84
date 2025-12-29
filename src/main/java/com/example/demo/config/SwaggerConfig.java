6bd7daba3aa3880741d403df8020d6fa097f5b8e
bala-amypo <bala@amypo.in>
2025-12-29 04:27:14 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -57,26 +57,39 @@
 
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI vehicleServiceAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
+
                 .servers(List.of(
-                    new Server().url("https://9213.pro604cr.amypo.ai"),
-                    new Server().url("http://localhost:9213")
+                        new Server().url("https://9213.pro604cr.amypo.ai/")
                 ))
-                .info(new Info()
-                        .title("Vehicle Service History API")
-                        .description("API for managing vehicle service history")
-                        .version("1.0"));
+         
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                );
     }
 }
