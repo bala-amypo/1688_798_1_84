1619f5338f35684f6316162bb822a59d8dac363c
bala-amypo <bala@amypo.in>
2025-12-29 04:56:11 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -57,39 +57,26 @@
 
 package com.example.demo.config;
 
-import java.util.List;
-
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI vehicleServiceAPI() {
         return new OpenAPI()
-
                 .servers(List.of(
-                        new Server().url("https://9213.pro604cr.amypo.ai/")
+                    new Server().url("https://9213.pro604cr.amypo.ai"),
+                    new Server().url("http://localhost:9213")
                 ))
-         
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        )
-                )
-
-                .addSecurityItem(
-                        new SecurityRequirement().addList("bearerAuth")
-                );
+                .info(new Info()
+                        .title("Vehicle Service History API")
+                        .description("API for managing vehicle service history")
+                        .version("1.0"));
     }
 }
